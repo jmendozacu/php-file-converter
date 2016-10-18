2f1de1def7462ee6560bb7a528f91bf27359f0e2
Brian Moniz <bcm811@gmail.com>
2016-10-18 10:58:02 -0600

    added a function that creates proper image filetype

--- convert.php
+++ convert.php
@@ -21,6 +21,38 @@ echo "not so much";
 }
 
 */
+
+// This function takes any of the main filetypes and with the switch statement creates a manipulatable image in the proper format (grabbed from php doc)
+function imageCreateFromAny($filepath) {
+    $type = exif_imagetype($filepath); // [] if you don't have exif you could use getImageSize()
+    $allowedTypes = array(
+        1,  // [] gif
+        2,  // [] jpg
+        3,  // [] png
+        6   // [] bmp
+    );
+    if (!in_array($type, $allowedTypes)) {
+        return false;
+    }
+    switch ($type) {
+        case 1 :
+            $im = imageCreateFromGif($filepath);
+        break;
+        case 2 :
+            $im = imageCreateFromJpeg($filepath);
+        break;
+        case 3 :
+            $im = imageCreateFromPng($filepath);
+        break;
+        case 6 :
+            $im = imageCreateFromBmp($filepath);
+        break;
+    }   
+    return $im; 
+}
+
+
+
 var_dump($_FILES['myfile']);
 $fileTmpName = $_FILES['myfile']['tmp_name'];
 $fileName = $_FILES['myfile']['name'];
@@ -30,14 +62,9 @@ $ext = $kaboom[1];
 
 
 
-switch ($ext){
-	case 'jpeg' : $image = imagecreatefromjpeg($fileTmpName);
-	break;
+$preppedImage = imageCreateFromAny($fileTmpName);
 
-	default : echo 'default running';
-};
-
-imagepng($image, 'tmp/newpng.png');
+imagepng($preppedImage, 'tmp/2newpng.png');
  //if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $target_file)) {
    //   echo "<P>FILE UPLOADED TO: $target_file</P>";
    //} else {
